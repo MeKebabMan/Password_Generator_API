16450ecaf8da62b492fd11af51501069aee34990
Lawen <lawenqader2010@gmail.com>
2024-05-25 21:11:32 +0200

    Second Push

--- src/Index.js
+++ src/Index.js
@@ -2,3 +2,14 @@ import express from "express";
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.json({
+        Message: "API!",
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`[API]: Connected to:\n http://localhost:${PORT}/`);
+});
